4d7a60dc8d5f06c47e96b05fb22d964f1474e0aa
Zers <zersxae@gmail.com>
2024-03-09 16:58:39 +0300

    hatalar düzeltildi

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 import "./style.scss";
 
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
 const PageNotFound = () => {
-  return (
-    <div>PageNotFound</div>
-  );
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Aradığınız şeyi bulamadık.</span>
+            </ContentWrapper>
+        </div>
+    );
 };
 
 export default PageNotFound;
